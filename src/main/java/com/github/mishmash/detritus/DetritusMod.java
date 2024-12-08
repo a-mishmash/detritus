8d4137678e06789941b5bbaa396083dd64d5a24e
a-mishmash <reldamail@icloud.com>
2024-12-08 15:49:13 -0800

    Worldgen changes

--- src/main/java/com/github/mishmash/detritus/DetritusMod.java
+++ src/main/java/com/github/mishmash/detritus/DetritusMod.java
@@ -1,10 +1,16 @@
 package com.github.mishmash.detritus;
 
+import com.github.mishmash.detritus.block.BlockRegistry;
+import com.github.mishmash.detritus.density_function.DensityFunctionRegistry;
 import net.fabricmc.api.ModInitializer;
 
 public class DetritusMod implements ModInitializer {
 
+    public static final String MOD_ID = "detritus";
+
     @Override
     public void onInitialize() {
+        BlockRegistry.initialize();
+        DensityFunctionRegistry.initialize();
     }
 }
